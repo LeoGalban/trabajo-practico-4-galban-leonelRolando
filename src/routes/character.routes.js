737dcf56571014e4d7bbb6c2ead1b87c56b1168b
Leonel Galban <leonelgalban13@gmail.com>
2025-06-27 05:33:19 -0300

    Se realizaron las rutas y se terminó el trabajo

---
+++ src/routes/character.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import {
+    getAllCharacters,
+    getCharacterById,
+    createCharacter,
+    updateCharacter,
+    deleteCharacter,
+} from "../controllers/character.controller.js";
+
+const router = Router();
+
+router.get("/", getAllCharacters);
+router.get("/:id", getCharacterById);
+router.post("/", createCharacter);
+router.put("/:id", updateCharacter);
+router.delete("/:id", deleteCharacter);
+
+export default router;
